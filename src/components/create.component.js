1dcbd4c939670d9e6cb150fc8efa1f25ce2cebad
Valentin <primarh111@mail.ru>
2020-01-27 10:10:34 +0300

    Добавлена валидация формы, обработка ошибок

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,40 @@
 import {mainComponent} from '../core/mainComponent'
+import {Form} from '../core/form'
+import {Validators} from '../core/validators'
 
 export class Create extends mainComponent {
     constructor (id) {
         super(id)
+
+        
     }
+
+    init (){
+        
+        this.$elem.addEventListener('submit', mySendForm.bind(this))
+        
+        this.Form = new Form(this.$elem, {
+            title:[Validators.required],
+            fulltext: [Validators.required, Validators.minLength(10)]
+        })
+        
+    }
+    
+}
+ 
+ 
+
+function mySendForm(event) {
+    event.preventDefault()
+    if(this.Form.isValid()){
+        const formData = {
+            type: this.$elem.type.value, 
+            fulltext: this.$elem.fulltext.value,
+            title: this.$elem.title.value
+        }
+        console.log(formData);
+        this.Form.clear()
+    } 
+    
+
 }
--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ const posts = new Posts('posts')
 const create = new Create('create')
 
 nav.registerTabs([
-    {name: 'create', component: create},
+    {name: 'create', component: create}, //name должен совпадать с дата атрибутом элемента в dom дереве
     {name: 'posts', component: posts},
     {name: 'favor', component: favor}
 ])
 
+    
 
 
 
